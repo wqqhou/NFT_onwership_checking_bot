0c835c91925f73d5b7835c26363e187de7c2616f
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 05:08:21 +0800

    init

--- db.py
+++ db.py
@@ -35,7 +35,7 @@ def check_address(wallet_address):
     duplicated = cur.fetchone()
     if duplicated:
         return unbond_address(duplicated[0])
-    return False
+    return True
 
 def set_address(uid, wallet_address):
     cur.execute(f'UPDATE Users SET addr = {wallet_address} WHERE uid = {uid}')
