23d76ce8342247598901b7ad45f7dd44983d1774
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 09:15:56 +0800

    init

--- check.py
+++ check.py
@@ -2,23 +2,24 @@ import requests
 import config
 import db
 import crypto
-import time
+import asyncio
 
-while True:
-    time.sleep(5)
-    try:
-        nft_resp = requests.get(f'https://tonapi.io/v2/nfts/collections/{config.NFT_CONTRACT}/items?'
-                                f'api_key= "{config.API_KEY}"').json()
-    except: 
-        continue
-    users = db.get_addresses()
-    for wallet_address in users:
-        ownership = False
-        for items in nft_resp['nft_items']:
-            if wallet_address[0] == crypto.account_forms(items['owner']['address']):
-                ownership = True
-                break
-        if not ownership:
-            uid = db.find_user(wallet_address[0])[0][0]
-            print(f'{uid} does not have the nft') 
+async def start():
+    while True:
+        asyncio.sleep(5)
+        try:
+            nft_resp = requests.get(f'https://tonapi.io/v2/nfts/collections/{config.NFT_CONTRACT}/items?'
+                                    f'api_key= "{config.API_KEY}"').json()
+        except: 
+            continue
+        users = db.get_addresses()
+        for wallet_address in users:
+            ownership = False
+            for items in nft_resp['nft_items']:
+                if wallet_address[0] == crypto.account_forms(items['owner']['address']):
+                    ownership = True
+                    break
+            if not ownership:
+                uid = db.find_user(wallet_address[0])[0][0]
+                print(f'{uid} does not have the nft') 
         
