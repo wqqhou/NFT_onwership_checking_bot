739fc1b269c3cfd58667808c06ac853a9dc3ff3e
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 09:36:24 +0800

    init

--- bot.py
+++ bot.py
@@ -136,6 +136,7 @@ async def main() -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    loop = asyncio.get_event_loop()
     asyncio.create_task(check.start())
     asyncio.run(main())
     
