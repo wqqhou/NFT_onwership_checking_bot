9c20e64b3a0568dae2fb8208143b334b67eaf4c0
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 08:53:48 +0800

    init

--- check.py
+++ check.py
@@ -12,10 +12,8 @@ while True:
     except: 
         continue
     users = db.get_addresses()
-    print(type(users))
+    print(users)
     for wallet_address in users:
-        wallet_address = str(wallet_address)
-        print(wallet_address)
         ownership = False
         for items in nft_resp['nft_items']:
             if wallet_address == crypto.account_forms(items['owner']['address']):
