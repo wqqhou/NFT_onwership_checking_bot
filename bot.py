0e206cb46e8f196acc1f40ee7d1d761d6d3d9bbb
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 09:39:53 +0800

    init

--- bot.py
+++ bot.py
@@ -131,7 +131,7 @@ async def main_callback_handler(call: CallbackQuery):
 async def main() -> None:
     await bot.delete_webhook(drop_pending_updates=True)  # skip_updates = True
     await dp.start_polling(bot)
-    asyncio.create_task(check.start())
+    await asyncio.create_task(check.start())
 
 
 
