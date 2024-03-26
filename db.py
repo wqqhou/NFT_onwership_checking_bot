39e68bc04632ce4b12a4cf77c94179092fe91244
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2024-03-26 08:35:25 +0800

    init

--- db.py
+++ db.py
@@ -28,7 +28,7 @@ def get_addresses():
 
 def find_user(wallet_address):
     cur.execute(f'SELECT uid FROM Users WHERE addr = "{wallet_address}"')
-    con.commit()
+    return cur.fetchone()
 
 def unbond_address(uid):
     cur.execute(f'UPDATE Users SET addr = "0x00" WHERE uid = {uid}')
